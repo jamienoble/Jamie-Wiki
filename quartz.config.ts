3ef7f96153933f34e8899b6891a28a68486ce761
Jamie <Jamie.Noble@cegeotech.co.uk>
2025-09-18 11:32:45 +0100

    + hide search bar and website name change

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jamie's Wiki",
+    pageTitle: "World Archive",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,10 +30,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
         { Component: Component.Darkmode() },
         { Component: Component.ReaderMode() },
       ],
@@ -56,10 +52,14 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+    // Remove or comment out the following line to hide explorer:
+    // Component.Explorer(),
+  ],
+  right: [],
+}
         { Component: Component.Darkmode() },
       ],
     }),
